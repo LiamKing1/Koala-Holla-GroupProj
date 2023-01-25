51f3640815242f44c2fc9444cfe46707c5134ccf
Liam King <techy.willyk3@gmail.com>
2023-01-24 21:39:48 -0600

    Added my database connection to my router file.

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pg = require('pg');
 
 // DB CONNECTION
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'Koala_Holla',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000,
+});
 
 
 // GET

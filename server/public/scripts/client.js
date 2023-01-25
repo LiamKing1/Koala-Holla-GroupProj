38701ee9da983751116139ffb748b89d194e1dc6
Liam King <techy.willyk3@gmail.com>
2023-01-24 21:54:54 -0600

    Sheesh, what a chunk! Got My GET, POST, PUT, and DELETE functions all working.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,40 +1,113 @@
-console.log( 'js' );
+console.log('js');
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
-  // Establish Click Listeners
-  setupClickListeners()
+// On page Load
+$(onReady);
+
+function onReady() {
+  $('#addButton').on('click', koalasPost);
+  $('#viewKoalas').on('click', '.update', updateKoala);
+  $('#viewKoalas').on('click', '.delete', deleteKoala);
   // load existing koalas on page load
   getKoalas();
+};
 
-}); // end doc ready
-
-function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+// PUT request Begins
+function updateKoala(){
+  let id = $(this).parent().parent().data('id');
+  let transferStatus = $(this).parent().parent().data('status');
+  if (transferStatus == 'y' || transferStatus == 'yes'){ 
+    transferStatus = 'no';
+  }else if (transferStatus == 'n' || transferStatus == 'no'){
+    transferStatus = 'yes';
+  };
+  console.log('in updateKoala');
+  $.ajax({
+    method: 'PUT',
+    url: `/koalas/ready_to_transfer/${id}`,
+    data: {status: transferStatus}
+  }).then((response) => {
+    console.log('db response', response);
+    getKoalas();
+  }).catch((error) => {
+    console.log('error updating data', error);
+  });
 }
+// PUT request Ends
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
+// DELETE request Begins
+function deleteKoala(){
+  console.log('In delete Koala button');
+  let id = $(this).parent().parent().data('id');
+  $.ajax({
+    method: 'DELETE',
+    url: `/koalas/${id}`,
+  }).then(() => {
+    getKoalas();
+  }).catch((error) => {
+    console.log('There is a problem with the DELETE function')
+  });
+}
+// DELETE request Ends
+
+// POST request Begins
+function koalasPost() {
+  console.log('in addButton on click');
+  let koalaToSend = {
+    name: $('#nameIn').val(),
+    age: $('#ageIn').val(),
+    gender: $('#genderIn').val(),
+    ready_to_transfer: $('#readyForTransferIn').val(),
+    notes: $('#notesIn').val()
+  };
+  $('#nameIn').val('');
+  $('#ageIn').val('');
+  $('#genderIn').val('');
+  $('#readyForTransferIn').val('');
+  $('#notesIn').val('');
   
-} // end getKoalas
+  $.ajax({
+    method: 'POST',
+    url: '/koalas',
+    data: koalaToSend
+  }).then(() => {
+    console.log('In POST function');
+    getKoalas();
+  }).catch((error) => {
+    console.log('Problem with POST function', error);
+  })
+};
+// POST request Ends
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
- 
-}
+// GET request Begins
+function getKoalas() {
+  console.log('in getKoalas');
+  $.ajax({
+    method: 'GET',
+    url: '/koalas'
+  }).then((response) => {
+    render(response);
+  }).catch((error) => {
+    console.log('Problem with GET function', error);
+  })
+};
+
+function render(koalaObject) {
+  console.log('In render function');
+  $('#viewKoalas').empty();
+  for (let object of koalaObject) {
+    $('#viewKoalas').append(`
+    <tr data-id="${object.id}" data-status="${object.ready_to_transfer}">
+      <td> ${object.name} </td>
+      <td> ${object.age} </td>
+      <td> ${object.gender} </td>
+      <td> ${object.ready_to_transfer} </td>
+      <td> ${object.notes} </td>
+      <td>
+        <button class="update"> UPDATE </button>
+        <button class="delete"> DELETE </button>
+      </td>
+    </tr>
+    `)
+  }
+};
+// GET request Ends
